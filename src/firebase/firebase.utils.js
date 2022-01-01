bbfeaf94d70bdfe8eb95623c6336b859397154c5
Linus Christian <44184209+lchristi@users.noreply.github.com>
2021-12-31 18:31:15 -0600

    Removing apikey

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,15 +2,7 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC95rZVCB0eF053MxzAFNmJhPPH-xLSJFA",
-  authDomain: "crwn-db-4a083.firebaseapp.com",
-  projectId: "crwn-db-4a083",
-  storageBucket: "crwn-db-4a083.appspot.com",
-  messagingSenderId: "499134906249",
-  appId: "1:499134906249:web:4f1818f18a4c4d444e6b58",
-  measurementId: "G-N7KJPKPZ87",
-};
+
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
